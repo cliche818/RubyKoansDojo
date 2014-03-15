623b854d44c7abf7aea62e77977b5dbb6adf3ed1
jeff <jeff.chun.yinli@gmail.com>
2014-03-15 17:03:14 -0400

    Wrote the implementation for triangle class to handle incorrect triangles

--- triangle.rb
+++ triangle.rb
@@ -14,12 +14,31 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+
+
+  handle_bad_triangle(a, b, c)
+
   if a == b && b == c
     :equilateral
   elsif a == b || b == c || a == c
     :isosceles
   else
     :scalene
+  end
+
+end
+
+def handle_bad_triangle(a, b, c)
+  if a == 0 || b == 0 || c == 0
+    raise TriangleError, "All sides must have a value"
+  elsif a < 0 || b < 0 || c < 0
+    raise TriangleError, "All sides must have a positive value"
+  elsif a + b <= c
+    raise TriangleError, "Triangle inequality : Triangle inequality"
+  elsif b + c <= a
+    raise TriangleError, "Triangle inequality : Triangle inequality"
+  elsif a + c <= b
+    raise TriangleError, "Triangle inequality : Triangle inequality"
   end
 end
 
